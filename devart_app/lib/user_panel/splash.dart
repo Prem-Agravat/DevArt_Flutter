6fe210487ec90cb2690834e89f0daae5c476b23c
Prem Agravat <agravatprem00@gmail.com>
2026-08-19 21:53:19 +0530

    feat: Add splash screen design

---
+++ devart_app/lib/user_panel/splash.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+// import 'package:devart/user_panel/login.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              'lib/assets/images/devart-logo.png',
+              width: 200,
+              height: 200,
+            ),
+            const SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: const [
+                Text(
+                  'Welcome to DevArt',
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black,
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                // Navigate to the login screen when the button is pressed
+                // Navigator.push(
+                //   context,
+                //   MaterialPageRoute(builder: (context) => const LoginScreen()),
+                // );
+              },
+              child: Text(
+                'Get Started',
+                style: TextStyle(fontSize: 18, color: Colors.white),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
