5e76b5da89b422196fa63e9191749efcb4e532f5
Prem Agravat <agravatprem00@gmail.com>
2026-08-24 20:51:46 +0530

    Integrate Firebase into the project by adding necessary configurations and dependencies; update build files, initialize Firebase in main.dart, and include Firebase options for multiple platforms.

--- devart_app/lib/main.dart
+++ devart_app/lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:devart/user_panel/splash.dart';
 import 'package:flutter/material.dart';
 //import 'package:devart/user_panel/splash.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
   runApp(const DevArtApp());
 }
 
