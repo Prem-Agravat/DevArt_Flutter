469260b654e36cdd14859e92d74c20d9a191f3c5
Prem Agravat <agravatprem00@gmail.com>
2026-09-07 10:40:10 +0530

    feat: initialize Firebase and configure root MaterialApp with splash screen entry point

--- devart_app/lib/main.dart
+++ devart_app/lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:devart/user_panel/splash.dart';
 import 'package:flutter/material.dart';
-//import 'package:devart/user_panel/splash.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
